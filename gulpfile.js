b185d252a529c06c70c291b436088495971753e5
Peter Sandor <peter@petersandor.name>
2015-07-11 01:11:17 +0200

    Fixed missing prettyprint styles

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var config = {
 	scripts: ['js/libs/modernizr.custom.js', 'js/libs/jquery-1.11.1.min.js', 'js/libs/jquery-migrate-1.2.1.min.js', 'js/libs/jquery.address-1.6.min.js', 'js/libs/triple.layout.js', 'js/libs/smoothscroll.js', 'js/libs/nprogress/nprogress.js', 'js/libs/fastclick.js', 'js/libs/jquery.imagesloaded.min.js', 'js/libs/isotope.pkgd.min.js', 'js/libs/twitterFetcher_v12_min.js', 'js/libs/jquery.fitvids.js', 'js/libs/jquery.validate.min.js', 'js/libs/jquery.uniform.min.js', 'js/libs/jquery.fancybox-1.3.4.pack.js', 'js/libs/jquery.debouncedresize.js', 'js/libs/classie.js', 'js/main.2.js'],
 	blogScripts: ['js/libs/modernizr.custom.js', 'js/libs/jquery-1.11.1.min.js', 'js/libs/jquery-migrate-1.2.1.min.js', 'js/libs/smoothscroll.js', 'js/libs/nprogress/nprogress.js', 'js/libs/fastclick.js', 'js/libs/jquery.fitvids.js', 'js/libs/jquery.fancybox-1.3.4.pack.js', 'js/libs/jquery.tooltipster.min.js', 'js/libs/google-code-prettify/prettify.js', 'js/blog.js'],
 	polyfills: ['js/libs/html5shiv.min.js', 'js/libs/respond.min.js', 'js/libs/selectivizr-min.js'],
-	styles: ['css/bootstrap.min.css', 'js/nprogress/nprogress.css', 'css/animate.min.css', 'css/font-awesome.min.css', 'css/fontello.css', 'css/jquery.fancybox-1.3.4.css', 'js/google-code-prettify/prettify.css', 'css/uniform.default.css', 'js/mediaelement/mediaelementplayer.css', 'css/tooltipster.css', 'css/main.css'],
+	styles: ['css/bootstrap.min.css', 'js/nprogress/nprogress.css', 'css/animate.min.css', 'css/font-awesome.min.css', 'css/fontello.css', 'css/jquery.fancybox-1.3.4.css', 'js/libs/google-code-prettify/prettify.css', 'css/uniform.default.css', 'js/mediaelement/mediaelementplayer.css', 'css/tooltipster.css', 'css/main.css'],
 	images: ['images/**/*'],
 	fonts: ['css/fonts/**/*']
 };
