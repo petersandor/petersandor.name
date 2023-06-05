acd72cdb4bc1b0d49d00ae3ef96e275f91cc218e
Peter Šándor <peter@petersandor.name>
2023-06-06 01:54:07 +0200

    chore: prettier IDE config fun

--- scripts/create-local-server.js
+++ scripts/create-local-server.js
@@ -1,20 +1,20 @@
-const { createServer: createHttpsServer } = require('https');
-const next = require('next');
-const fs = require('fs');
-const chalk = import('chalk');
+const { createServer: createHttpsServer } = require('https')
+const next = require('next')
+const fs = require('fs')
+const chalk = import('chalk')
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const PORT = process.env.PORT || 3000
 
 if (!fs.existsSync('./certs/.capath')) {
-  console.error(chalk.red('\nError: Missing SSL certificates\n'));
+  console.error(chalk.red('\nError: Missing SSL certificates\n'))
 
-  console.error(`To fix this error, run the command below:`);
-  console.error(`→ npm run ssl:setup\n`);
+  console.error(`To fix this error, run the command below:`)
+  console.error(`→ npm run ssl:setup\n`)
 
-  process.exit();
+  process.exit()
 }
 
 app
@@ -26,14 +26,14 @@ app
         cert: fs.readFileSync('./certs/devcert.cert'),
       },
       (req, res) => handle(req, res)
-    );
+    )
 
     return server.listen(PORT, (err) => {
-      if (err) throw err;
+      if (err) throw err
 
-      console.log('> Ready on https://my-cool-domain.local:3000')
-    });
+      console.log('> Ready on port 3000')
+    })
   })
   .catch((err) => {
-    console.error(err);
-  });
+    console.error(err)
+  })
