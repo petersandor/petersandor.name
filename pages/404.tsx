1acf4d5fd26f298ec2baace8dde4980ba2c03c92
Peter Šándor <peter@petersandor.name>
2023-07-11 02:51:25 +0200

    updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ export default function FourZeroFour() {
       <PageSEO title="Page Not Found" description="Sorry we couldn't find this page :(" />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+          <h1 className="text-shadow-xl text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
             404
           </h1>
         </div>
@@ -16,7 +16,7 @@ export default function FourZeroFour() {
             Sorry we couldn't find this page.
           </p>
           <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
+            But dont worry, you can find plenty of other things on the homepage.
           </p>
           <Link
             href="/"
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,7 +30,10 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
-        <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
+        <body
+          className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white"
+          data-page={this.props.__NEXT_DATA__.page}
+        >
           <Main />
           <NextScript />
         </body>
