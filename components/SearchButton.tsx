c8144cc153245a49950705c7029c9abdac71bcea
Peter Šándor <peter@petersandor.name>
2024-02-11 22:49:52 +0100

    revert: device detection attempt

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,26 +1,8 @@
 import { AlgoliaButton } from 'pliny/search/AlgoliaButton'
 import { KBarButton } from 'pliny/search/KBarButton'
 import siteMetadata from '@/data/siteMetadata'
-import { useDeviceSelectors } from 'react-device-detect'
-
-const useDeviceDetection = () => {
-  const userAgent =
-    '__incrementalCache' in globalThis
-      ? globalThis.__incrementalCache.requestHeaders['user-agent']
-      : ''
-
-  const deviceData = useDeviceSelectors(userAgent)
-
-  if (Array.isArray(deviceData)) {
-    return deviceData.at(0)
-  }
-
-  return {}
-}
 
 const SearchButton = () => {
-  const { isDesktop, isMacOs } = useDeviceDetection()
-
   if (
     siteMetadata.search &&
     (siteMetadata.search.provider === 'algolia' || siteMetadata.search.provider === 'kbar')
@@ -44,19 +26,13 @@ const SearchButton = () => {
             d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
           />
         </svg>
-        {isDesktop && (
-          <>
-            <kbd className="ml-1 inline-block whitespace-nowrap rounded border px-1.5 align-middle font-medium leading-4 tracking-wide text-xs text-gray-400 border-gray-400">
-              {isMacOs ? '⌘' : 'Ctrl'}
-            </kbd>
-            <span className="font-medium leading-4 tracking-wide text-xs text-gray-400 mx-1">
-              +
-            </span>
-            <kbd className="inline-block whitespace-nowrap rounded border px-1.5 align-middle font-medium leading-4 tracking-wide text-xs text-gray-400 border-gray-400">
-              K
-            </kbd>
-          </>
-        )}
+        <kbd className="ml-1 inline-block whitespace-nowrap rounded border px-1.5 align-middle font-medium leading-4 tracking-wide text-xs text-gray-400 border-gray-400">
+          ⌘
+        </kbd>
+        <span className="font-medium leading-4 tracking-wide text-xs text-gray-400 mx-1">+</span>
+        <kbd className="inline-block whitespace-nowrap rounded border px-1.5 align-middle font-medium leading-4 tracking-wide text-xs text-gray-400 border-gray-400">
+          K
+        </kbd>
       </SearchButtonWrapper>
     )
   }
