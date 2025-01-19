a167248b5e06c71d837397cdc7dd480f668f3805
Peter Šándor <peter@petersandor.name>
2025-01-19 02:14:34 +0100

    chore: use react-tweet to embed Twitter/X posts'

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://*.twitter.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app https://*.twitter.com
+  frame-src giscus.app
 `
 
 const securityHeaders = [
